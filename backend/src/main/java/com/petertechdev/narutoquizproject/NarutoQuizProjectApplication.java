a2353210451f76dd131750cd0c6084b738c6323b
petertechdev <peterleite.dev@gmail.com>
2023-10-02 02:44:55 -0300

    Initial backend setup with Spring Boot

---
+++ backend/src/main/java/com/petertechdev/narutoquizproject/NarutoQuizProjectApplication.java
@@ -0,0 +1,13 @@
+package com.petertechdev.narutoquizproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoQuizProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoQuizProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/petertechdev/narutoquizproject/NarutoQuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petertechdev.narutoquizproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoQuizProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
